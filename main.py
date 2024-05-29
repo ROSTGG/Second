bee2d9843f78da990fe5121611252184606ffe89
ROSTGG <rm9261470349@gmail.com>
2024-05-29 20:16:48 +0300

    000005

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import asyncio
 
-from Telegram.bd_functions.bd import isRegisterUser
+# from Telegram.bd_functions.bd import isRegisterUser
 from Telegram.bot import main
 # from Telegram.bot_dialogs.data import bot_for_test
-
+# from rich.console import Console
+# from rich.text import Text
+#
+# console = Console()
+#
+# console.print("Этот текст [bold red]красный и жирный[/bold red]")
+# console.print("Этот текст [green on yellow]зеленый на желтом фоне[/green on yellow]")
+# console.print("Этот текст [underline blue]с подчеркиванием[/underline blue]")
+# console.print("Этот текст [bold magenta blink]мигает и жирный[/bold magenta blink]")
 
 if __name__ == '__main__':
     asyncio.run(main())
